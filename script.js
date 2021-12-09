349dfaa70931b3e886f788d3087ca7009a0ef0ec
Cyb_detective <66636466+cipher387@users.noreply.github.com>
2021-12-10 01:15:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [35.00, 1.00],
+        zoom: 4
+    }, {
+        searchControlProvider: 'yandex#search'
+    });
+myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+		document.getElementById("latitude").value = [coords[0].toPrecision(9)];
+		document.getElementById("longitude").value = [coords[1].toPrecision(9)];
+            myMap.balloon.open(coords, {
+                contentHeader:'Coordinates:',
+                contentBody:'<p>' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ') + '</p>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+}
+
 function buttonOpen(buttonValue) {
 
     latitude = document.getElementById("latitude").value;
